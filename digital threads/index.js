34c9719edf3a6752753a337b2e3b2a7e64a3f645
AarzooKaushik <aarzookaushik31@gmail.com>
2022-12-11 14:55:27 +0530

    aarzoo

--- digital threads/index.js
+++ digital threads/index.js
@@ -1,3 +1,7 @@
+/**********************************/
+/********* Sticky Navbar **********/
+/**********************************/
+
 const homeContainer = document.querySelector(".home-container");
 navbar = document.querySelector(".navbar");
 
@@ -18,3 +22,17 @@ const observer = new IntersectionObserver(
   }
 );
 observer.observe(homeContainer);
+
+/**********************************/
+/********* Smooth Scroll **********/
+/**********************************/
+
+const allLinks = document.querySelectorAll("a:link");
+allLinks.forEach(function (link) {
+  link.addEventListener("click", function (e) {
+    e.preventDefault();
+    const href = link.getAttribute("href");
+    const sectionEl = document.querySelector(href);
+    sectionEl.scrollIntoView({ behavior: "smooth" });
+  });
+});
